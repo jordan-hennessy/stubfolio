db66f3f559f28725e8f9ba3eca083c4800de4a2b
Jordan Hennessy <hennessyjordan@outlook.com>
2026-07-09 13:52:41 +0100

    Add: parse_setlist to shape raw setlist.fm data for Concert/Song creation

--- apps/concerts/services.py
+++ apps/concerts/services.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 from django.conf import settings
 
 SETLISTFM_BASE_URL = "https://api.setlist.fm/rest/1.0"
@@ -45,3 +46,43 @@ def find_artist_exact_match(artist_name):
             return artist
 
     return None
+
+
+def parse_setlist(raw_setlist):
+    """
+    Take a raw setlist dict from the setlist.fm API and shape it into
+    clean data ready to create a Concert + Song rows from.
+    """
+    event_date = datetime.strptime(raw_setlist["eventDate"], "%d-%m-%Y").date()
+
+    concert_data = {
+        "artist_name": raw_setlist["artist"]["name"],
+        "musicbrainz_id": raw_setlist["artist"]["mbid"],
+        "venue_name": raw_setlist["venue"]["name"],
+        "city": raw_setlist["venue"]["city"]["name"],
+        "country": raw_setlist["venue"]["city"]["country"]["name"],
+        "date": event_date,
+        "setlistfm_id": raw_setlist["id"],
+    }
+
+    songs_data = []
+    position = 1
+
+    for set_block in raw_setlist["sets"]["set"]:
+        is_encore = "encore" in set_block
+
+        for song in set_block.get("song", []):
+            songs_data.append(
+                {
+                    "title": song["name"],
+                    "info": song.get("info", ""),
+                    "position": position,
+                    "is_encore": is_encore,
+                }
+            )
+            position += 1
+
+    return {
+        "concert": concert_data,
+        "songs": songs_data,
+    }

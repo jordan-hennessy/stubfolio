79437c54cb6c3d6fb983db352217811bb9dc0f86
Jordan Hennessy <hennessyjordan@outlook.com>
2026-07-30 22:21:23 +0100

    Replace ConcertsPage with MyCollectionPage in routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import ConcertsPage from "./pages/ConcertsPage";
+import MyCollectionPage from "./pages/MyCollectionPage";
 import AddConcertPage from "./pages/AddConcertPage";
 import ConcertDetailPage from "./pages/ConcertDetailPage";
 
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/concerts" element={<ConcertsPage />} />
+        <Route path="/concerts" element={<MyCollectionPage />} />
         <Route path="/concerts/add" element={<AddConcertPage />} />
         <Route path="/concerts/:id" element={<ConcertDetailPage />} />
       </Routes>

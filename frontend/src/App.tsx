029beb22e714c90dac77654731f90c114c8480ab
Jordan Hennessy <hennessyjordan@outlook.com>
2026-08-10 17:32:10 +0100

    Add signup route to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 import MyCollectionPage from "./pages/MyCollectionPage";
 import AddConcertPage from "./pages/AddConcertPage";
 import ConcertDetailPage from "./pages/ConcertDetailPage";
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/concerts" element={<MyCollectionPage />} />
         <Route path="/concerts/add" element={<AddConcertPage />} />
         <Route path="/concerts/:id" element={<ConcertDetailPage />} />

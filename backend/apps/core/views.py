a8362d6afda3562af79437ff26bd4cde84aa9d80
Jordan Hennessy <hennessyjordan@outlook.com>
2026-08-19 14:59:39 +0100

    Allow HEAD requests on health check endpoint

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -2,6 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(["GET"])
+@api_view(["GET", "HEAD"])
 def health(request):
     return Response({"status": "ok"})
